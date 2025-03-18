e2a6f20367a1c8d6c945759102096463f420ef63
drakkomaximo <andres.villanueva.tabares@gmail.com>
2025-03-18 14:59:31 -0500

    First changes

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,12 +1,32 @@
-import Header from "./_components/Header";
-import HotDeal from "./_components/HotDeal";
-
+const items = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "how-it-works",
+    title: "How It Works",
+  },
+  {
+    id: "rewards",
+    title: "Rewards",
+  },
+  {
+    id: "faq",
+    title: "FAQ",
+  },
+];
 const Home = () => {
   return (
     <div className="min-h-screen">
-      <div className="container mx-auto px-4 py-12">
-        <Header />
-        <HotDeal />
+      <div className="container mx-auto">
+        {
+          items.map((item) => (
+            <div key={item.id} id={item.id} className="bg-white rounded-lg p-4 shadow-md h-[500px] mb-8">
+              <h1 className="text-2xl font-bold text-center">{item.title}</h1>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
