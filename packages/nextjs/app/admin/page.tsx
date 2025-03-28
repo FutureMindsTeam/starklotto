ac1c282e71331ab1b508503bf37192a837f0d9a8
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-03-28 08:17:33 +0100

    refactor: added the component to the admin dashboard

--- packages/nextjs/app/admin/page.tsx
+++ packages/nextjs/app/admin/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "~~/components/ui/button";
 import SweepstakesModal from "~~/components/sweepstakes";
 import { useSweepstakesStore } from "~~/services/store/sweepstakesStore";
 import ContentDisplay from "~~/components/sweepstakes/ContentDisplay";
+import SettingsCard from "~~/components/admin-panel/SettingsCard";
 
 export default function AdminPage() {
   const { openModal } = useSweepstakesStore();
@@ -13,6 +14,7 @@ export default function AdminPage() {
       <h2 className="text-3xl font-semibold">Admin Page</h2>
       <Button onClick={openModal}>Set Draw Parameters</Button>
       <ContentDisplay />
+      <SettingsCard/>
       <SweepstakesModal modalId="sweepstakesModal" />
     </div>
   );
